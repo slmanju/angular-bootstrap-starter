a0c9cb43a3ad7794a532ffd9f5f9cc7dfa3ca057
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2017-12-02 21:06:22 +0530

    separate header navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppBootstrapModule } from './app-bootstrapcss.module';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './app.header';
 
 @NgModule({
   declarations: [
-    AppComponent
+    [AppComponent, HeaderComponent]
   ],
   imports: [
     [BrowserModule, AppBootstrapModule, AngularFontAwesomeModule]
